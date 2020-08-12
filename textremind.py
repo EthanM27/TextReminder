2faa83eff3d879b1d7f6a8d042485a19145cd16a
EthanM27 <eman2201@gmail.com>
2020-08-11 20:28:17 -0400

    finished send messages at specific times. Wait to see if it worked

--- textremind.py
+++ textremind.py
@@ -1,9 +1,9 @@
 import os
-import sched
-from twilio.rest import Client
-import datetime
+import time
+from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from twilio.rest import Client
 
 #twilio setup
 ethan_account = {'accountSID': os.environ['TWILIO_ACCOUNT_SID'], 'Auth Token': os.environ['TWILIO_AUTH_TOKEN']}
@@ -16,15 +16,48 @@ ethanCell = text[1].strip()
 mathieuCell = text[2].strip()
 
 #google drive file setup
-
+scope = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name('./client_secret.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open("Education and Fitness Tracking").sheet1
 
 #sending text messages
-#mat_message = client.messages.create(body='Test python message', from_=myTwilioNumber, to=mathieuCell)
-#ethan_message = client.messages.create(body='Test python message', from_=myTwilioNumber, to=ethanCell)
 
-def testtime():
-    x = datetime.datetime.now()
-    print(x)
+
+def send_message():
+    workout_sent = False
+    coding_sent = False
+    while datetime.now().strftime('%H'):
+        hour = datetime.now().strftime('%H')
+
+        if hour == "10":
+            if not coding_sent:
+                coding_remind()
+                coding_sent = True
+        elif (hour == "17") or (hour == "19"):
+            if not workout_sent:
+                workout_remind()
+                workout_sent = True
+            
+        if hour == "11":
+            coding_sent = False
+        elif (hour == "18") or (hour == "20"):
+            workout_sent = False
+        
+        time.sleep(5)
+        print("loop executed at:", datetime.now())
+        
+            
+def coding_remind():
+    mat_message = client.messages.create(body='Remember to do at least one hour of coding practice today', from_=myTwilioNumber, to=mathieuCell)
+    ethan_message = client.messages.create(body='Remember to do at least one hour of coding practice today', from_=myTwilioNumber, to=ethanCell)
+
+def workout_remind():
+    mat_message = client.messages.create(body='Remember to workout today', from_=myTwilioNumber, to=mathieuCell)
+    ethan_message = client.messages.create(body='Remember to workout today', from_=myTwilioNumber, to=ethanCell)
 
 if __name__ == "__main__":
-    testtime()
+    send_message()
